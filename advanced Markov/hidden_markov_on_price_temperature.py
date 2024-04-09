47fcac3aa786d4c31574f0b3aa13bbb1ebe9c515
xyfyyds <1109232044@qq.com>
2024-04-09 15:36:31 +0800

    update

--- advanced Markov/hidden_markov_on_price_temperature.py
+++ advanced Markov/hidden_markov_on_price_temperature.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from hmmlearn import hmm
+from matplotlib import pyplot as plt
 
 # 从CSV文件中读取数据
 file_path = '../data_generated/weather/temperature_price_states.csv'  # 替换为你的CSV文件路径
@@ -52,32 +53,30 @@ mask = (observations_matrix != 0) & (observations_matrix != 1)
 print(np.count_nonzero(mask))
 
 # select the initial state probability vector 2020.9.28 23:00
-pi = transition_matrix[np.where(unique_states == 9.4)[0][0]]
+pi = transition_matrix[np.where(unique_states == 1.9)[0][0]]
 print(pi)
 
 
-# HMM model by python package hmmlearn
-model = hmm.CategoricalHMM(n_components=num_states)
-model.startprob_ = pi
-model.transmat_ = transition_matrix
-model.emissionprob_ = observations_matrix
-model.n_features = num_observations
-ob_list = [[2, 3], [2, 56], [12,2]]
-for i in ob_list:
-    prob = model.score([i])
-    print(f"log_prob_{i}:", prob)
+# # HMM model by python package hmmlearn
+# model = hmm.CategoricalHMM(n_components=num_states)
+# model.startprob_ = pi
+# model.transmat_ = transition_matrix
+# model.emissionprob_ = observations_matrix
+# model.n_features = num_observations
+# ob_list = [[2, 3], [2, 56], [12,2]]
+# for i in ob_list:
+#     prob = model.score([i])
+#     print(f"log_prob_{i}:", prob)
 
 
-# forward algorithm
-obs_seq = [2,3]
-def forward():
+def forward(obs_seq):
     alpha = pi * observations_matrix[:, obs_seq[0]]
     for obs in obs_seq[1:]:
         alpha = np.dot(alpha, transition_matrix) * observations_matrix[:, obs]
     return np.sum(alpha)
 
 
-def backward():
+def backward(obs_seq):
     obs_len = len(obs_seq)
     beta = np.ones_like(observations_matrix[:, 0])
     for i in list(range(0, obs_len - 1))[::-1]:
@@ -85,7 +84,72 @@ def backward():
         beta = np.dot(transition_matrix, observations_matrix[:, obs] * beta)
     return np.sum(pi * observations_matrix[:, obs_seq[0]] * beta)
 
-res_forward = forward()
-print(f"res_forward={res_forward}")
-res_backward = backward()
-print(f"res_backward={res_backward}")
+# res_forward = forward(obs_seq)
+# print(f"res_forward={res_forward}")
+# res_backward = backward(obs_seq)
+# print(f"res_backward={res_backward}")
+
+# possible_observations = []
+# for i in range(num_observations):
+#     for j in range(num_observations):
+#         for k in range(num_observations):
+#             possible_observations.append([i, j, k])
+
+# max_prob = -np.inf
+# max_ob = []
+# for ob in possible_observations:
+#     prob = model.score([ob])
+#     if prob > max_prob:
+#         max_prob = prob
+#         max_ob = ob
+#
+# print(f"max_prob={max_prob}, max_ob={max_ob}")
+# for item in max_ob:
+#     print(unique_observations[item])
+
+pi_list = []
+for item in states[:1000]:
+    pi = transition_matrix[np.where(unique_states == item)[0][0]]
+    pi_list.append(pi)
+
+possible_observations = []
+for i in range(num_observations):
+        possible_observations.append([i])
+
+result_list = []
+
+for pi in pi_list:
+    max_prob = -np.inf
+    max_ob = []
+    for ob in possible_observations:
+        prob = forward(ob)
+        if prob > max_prob:
+            max_prob = prob
+            max_ob = ob
+    result_list.append(unique_observations[max_ob[0]])
+
+real_data = data['price_state'][:1001]
+
+overall_difference = 0
+count = 0
+for i in range(len(result_list)):
+    diff = abs(result_list[i] - real_data[i+1])
+    overall_difference += diff
+    if diff <= abs(real_data[i] * 0.2):
+        count += 1
+
+print("Average Difference: ", overall_difference / len(result_list))
+print("Percentage of Difference within 20%: ", str((count / len(result_list)) * 100) + "%" )
+
+plt.figure(figsize=(20, 12))
+# Plot the predicted states
+plt.plot(result_list, label='simulation')
+# Plot the original states
+plt.plot(real_data[1:], label='real price')
+
+plt.xlabel('Times of transitions')
+plt.ylabel('Price')
+plt.title('Comparison')
+plt.legend()
+
+plt.show()
--- advanced Markov/hidden_markov_on_solar_power.py
+++ advanced Markov/hidden_markov_on_solar_power.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from hmmlearn import hmm
+from matplotlib import pyplot as plt
 
 file_path = '../data_generated/weather/solar_power_states.csv'
 df = pd.read_csv(file_path)
@@ -72,27 +73,27 @@ pi = transition_matrix_list[10][np.where(unique_states == 325)[0][0]]
 print(pi)
 
 
-# HMM model by python package hmmlearn
-model = hmm.CategoricalHMM(n_components=num_states)
-model.startprob_ = pi
-model.transmat_ = transition_matrix_list[11]
-model.emissionprob_ = observations_matrix
-model.n_features = num_observations
-ob_list = [15]
-print(unique_observations[ob_list[0]])
-prob = model.score([ob_list])
-print(f"log_prob_{ob_list}:", prob)
+# # HMM model by python package hmmlearn
+# model = hmm.CategoricalHMM(n_components=num_states)
+# model.startprob_ = pi
+# model.transmat_ = transition_matrix_list[11]
+# model.emissionprob_ = observations_matrix
+# model.n_features = num_observations
+# ob_list = [15]
+# print(unique_observations[ob_list[0]])
+# prob = model.score([ob_list])
+# print(f"log_prob_{ob_list}:", prob)
 
 
 # forward algorithm
-def forward():
+def forward(ob_list):
     alpha = pi * observations_matrix[:, ob_list[0]]
     for obs in ob_list[1:]:
         alpha = np.dot(alpha, transition_matrix) * observations_matrix[:, obs]
     return np.sum(alpha)
 
 
-def backward():
+def backward(ob_list):
     obs_len = len(ob_list)
     beta = np.ones_like(observations_matrix[:, 0])
     for i in list(range(0, obs_len - 1))[::-1]:
@@ -100,7 +101,53 @@ def backward():
         beta = np.dot(transition_matrix, observations_matrix[:, obs] * beta)
     return np.sum(pi * observations_matrix[:, ob_list[0]] * beta)
 
-res_forward = forward()
-print(f"res_forward={res_forward}")
-res_backward = backward()
-print(f"res_backward={res_backward}")
+
+
+pi_list = []
+initial_hour = 12
+for item in df[df['Hour'] == initial_hour]['solar_power_states'].values:
+    pi = transition_matrix_list[initial_hour][np.where(unique_states == item)[0][0]]
+    pi_list.append(pi)
+
+possible_observations = []
+for i in range(num_observations):
+    possible_observations.append([i])
+
+result_list = []
+
+for pi in pi_list:
+    max_prob = -np.inf
+    max_ob = []
+    for ob in possible_observations:
+        prob = forward(ob)
+        if prob > max_prob:
+            max_prob = prob
+            max_ob = ob
+    result_list.append(unique_observations[max_ob[0]])
+
+real_data = df[df['Hour'] == (initial_hour + 1)]['pv_states'].values
+
+overall_difference = 0
+count = 0
+print(len(result_list))
+for i in range(len(result_list) - 1):
+    diff = abs(result_list[i] - real_data[i+1])
+    overall_difference += diff
+    if diff <= abs(real_data[i] * 0.2):
+        count += 1
+
+print("Average Difference: ", overall_difference / len(result_list))
+print("Percentage of Difference within 20%: ", str((count / len(result_list)) * 100) + "%" )
+
+plt.figure(figsize=(20, 12))
+# Plot the predicted states
+plt.plot(result_list, label='simulation')
+# Plot the original states
+plt.plot(real_data[1:], label='real data')
+
+plt.xlabel('Times of transitions')
+plt.ylabel('Power')
+plt.title('Comparison')
+plt.legend()
+
+plt.show()
